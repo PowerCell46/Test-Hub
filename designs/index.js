95a14be6e211a8839402851d674c3816559f9c5d
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2024-02-27 01:48:31 +0200

    finished with the main design of the home page

--- designs/index.js
+++ designs/index.js
@@ -1,6 +1,6 @@
 function hideShowNavigation(e) {
     const navigation = document.querySelector("#navigation-section");
-    
+
     if (navigation.style.display === 'none') {
         e.target.style.transform = 'rotate(90deg)';
         navigation.style.display = 'flex';
@@ -10,5 +10,20 @@ function hideShowNavigation(e) {
         e.target.style.transform = 'rotate(0deg)';
         navigation.style.display = 'none';
         navigation.parentElement.style.margin = '0';
+    }
+}
+
+
+function hideShowTopics(e) {
+    const hiddenTopics = e.target.parentElement.querySelector(".hidden");
+    
+    if (hiddenTopics.style.display == 'none') {
+        console.log(hiddenTopics);
+        e.target.parentElement.querySelector(".fa-arrow-down").style.transform = 'rotate(180deg)';
+        hiddenTopics.style.display = 'block';
+    
+    } else {
+        e.target.parentElement.querySelector(".fa-arrow-down").style.transform = 'rotate(0deg)';
+        hiddenTopics.style.display = 'none';
     }
 }
