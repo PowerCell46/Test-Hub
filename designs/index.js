2281ded83e111273b52fe0717303cd8b68a62335
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2024-02-28 21:02:28 +0200

    added animation to the header + other cosmetic changes

--- designs/index.js
+++ designs/index.js
@@ -1,27 +1,26 @@
 function hideShowNavigation(e) {
     const navigation = document.querySelector("#navigation-section");
-
     if (navigation.style.display === 'none') {
         e.target.style.transform = 'rotate(90deg)';
         navigation.style.display = 'flex';
-        navigation.parentElement.style.margin = '3vh 0'
+        setTimeout(() => navigation.style.opacity = 1, 250);
 
     } else {
         e.target.style.transform = 'rotate(0deg)';
-        navigation.style.display = 'none';
-        navigation.parentElement.style.margin = '0';
+        navigation.style.opacity = 0;
+        setTimeout(() => navigation.style.display = 'none', 500);
     }
 }
 
 
 function hideShowTopics(e) {
     const hiddenTopics = e.target.parentElement.querySelector(".hidden");
-    
+
     if (hiddenTopics.style.display == 'none') {
         console.log(hiddenTopics);
         e.target.parentElement.querySelector(".fa-arrow-down").style.transform = 'rotate(180deg)';
         hiddenTopics.style.display = 'block';
-    
+
     } else {
         e.target.parentElement.querySelector(".fa-arrow-down").style.transform = 'rotate(0deg)';
         hiddenTopics.style.display = 'none';
