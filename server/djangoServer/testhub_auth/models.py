5f49b093883ade3377be1be22312fdd72c231887
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2024-03-07 20:06:04 +0200

    created the design for the my profile page and created the foundation for the edit profile view

--- server/djangoServer/testhub_auth/models.py
+++ server/djangoServer/testhub_auth/models.py
@@ -7,6 +7,9 @@ from django.dispatch import receiver
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='user_images/')  # , default='default.jpg' ADD A DEFAULT IMAGE IN THE FOLDER!
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
+    nationality = models.CharField(max_length=25, null=True, blank=True)
+    gender = models.CharField(choices=(('Male', 'Male'), ('Female', 'Female')), null=True, blank=True)
 
     def __str__(self):
         return self.user.username
