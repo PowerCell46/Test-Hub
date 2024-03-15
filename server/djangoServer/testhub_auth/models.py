6253e7a3d16669bc2824a2b0988249b1733452df
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2024-03-15 10:38:44 +0200

    laptop media queries

--- server/djangoServer/testhub_auth/models.py
+++ server/djangoServer/testhub_auth/models.py
@@ -9,7 +9,7 @@ class UserProfile(models.Model):
     image = models.ImageField(upload_to='user_images/', default='user_images/defaultProfilePic.png')
     phone_number = models.CharField(max_length=10, null=True, blank=True)
     nationality = models.CharField(max_length=25, null=True, blank=True)
-    gender = models.CharField(choices=(('Male', 'Male'), ('Female', 'Female')), null=True, blank=True)
+    gender = models.CharField(choices=(('Male', 'Male'), ('Female', 'Female')), null=True, blank=True, max_length=20)
     is_deleted = models.BooleanField(default=False)
 
     def soft_delete(self):
